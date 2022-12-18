0dbf623a96b9d35a5df0e64e43be5089cb317c9c
Michael Snook <michael.snook@gmail.com>
2022-12-18 17:23:29 +0530

    Install tailwind v3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,jsx,css}',
+    './components/**/*.js',
+    './styles/globals.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
